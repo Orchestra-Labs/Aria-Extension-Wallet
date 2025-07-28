09dfae1c10f70929f16e713f54d3852d174e81e3
dblackstone1 <dblackstone@orchestralabs.org>
2025-07-28 16:29:11 -0400

    chore: amend todo

--- src/pages/main/WalletConnectLoader/WalletConnectLoader.tsx
+++ src/pages/main/WalletConnectLoader/WalletConnectLoader.tsx
@@ -5,6 +5,6 @@ import React from 'react';
 import { ScreenLoader } from '@/components';
 
 export const WalletConnectLoader: React.FC = () => {
-  // TODO maybe set timeout for a 1 min, if no connection established, close the window?
+  // TODO: maybe set timeout for a 1 min, if no connection established, close the window?
   return <ScreenLoader />;
 };
