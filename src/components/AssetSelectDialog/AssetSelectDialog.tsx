8799696b8a6c7bc98d39dbd6be6b838486f92eb0
David Blackstone <dblackstone@orchestralabs.org>
2025-11-29 11:09:14 -0500

    fix: default sort order is ASC, not DESC

--- src/components/AssetSelectDialog/AssetSelectDialog.tsx
+++ src/components/AssetSelectDialog/AssetSelectDialog.tsx
@@ -64,7 +64,7 @@ export const AssetSelectDialog: React.FC<AssetSelectDialogProps> = ({
 
   const resetDefaults = () => {
     setSearchTerm('');
-    setSortOrder(SortOrder.DESC);
+    setSortOrder(SortOrder.ASC);
     setSortType(AssetSortType.NAME);
   };
 
